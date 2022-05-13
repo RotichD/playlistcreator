09d3b7bd9c7d5ba857d37633db542d1cae888901
4FRO <drotich@ucsc.edu>
2022-05-13 14:47:04 -0700

    added like button and + button to track, styled search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
+import Searchbar from "./components/searchbar/Searchbar";
+import Results from "./components/results/Results";
 import Footer from "./components/footer/Footer";
-import Results from './components/results/Results';
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <Searchbar />
       <Results />
       <Footer />
     </div>
--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./track.css";
 
 export const Track = () => {
+  const [like, setLike] = useState(false);
+
   return (
     <div className="track">
       <div className="image">
@@ -13,8 +15,48 @@ export const Track = () => {
       <div className="information">
         <p className="title">Touch The Sky</p>
         <p className="explicit">E</p>
+        <button className="like" onClick={() => setLike(!like)}>
+          {like ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-heart-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-heart"
+              viewBox="0 0 16 16"
+            >
+              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+            </svg>
+          )}
+        </button>
         <p className="artist">Kanye West</p>
         <p className="trackTime">3:56</p>
+        <button className="add">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-plus"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg>
+        </button>
       </div>
     </div>
   );
