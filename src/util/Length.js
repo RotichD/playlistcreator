54a0808fe473012d766589ddf418acec91554b1c
4FRO <drotich@ucsc.edu>
2022-05-19 17:07:42 -0700

    fixed state issue with tracks, fixed footer

--- src/util/Length.js
+++ src/util/Length.js
@@ -3,7 +3,11 @@
 export const formatTime = (ms) => {
     const seconds = ms/1000;
     const minutes = Math.floor(seconds / 60); 
-    const secondsRemainder = Math.floor(seconds % 60);
+    let secondsRemainder = Math.floor(seconds % 60);
+    if (secondsRemainder < 10) {
+        return `${minutes}:0${secondsRemainder}`;
+    }
+    
     return `${minutes}:${secondsRemainder}`;
 }
 
