fcb41bb8e3d6cb9d7974c42ab943d34df176302b
4FRO <drotich@ucsc.edu>
2022-05-31 18:28:48 -0700

    Conditionally Renders Search instructions

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -9,6 +9,9 @@ export const Results = ({ onAdd, searchResults }) => {
     <>
       <div className='results-container'>
         {searchResults.length > 0 && <h2>Results</h2>}
+        {searchResults.length == 0 && (
+          <h2>Search for an Artist, Song, or Album to begin</h2>
+        )}
         <Tracklist tracks={searchResults} onAdd={onAdd} isRemoval={false} />
       </div>
     </>
